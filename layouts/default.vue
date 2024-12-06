d2a0a17343df85aa503bdd069585e93af7713d47
diffident016 <marvin.tagolimotjr13@gmail.com>
2024-12-06 21:46:04 +0800

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app>
+    <NuxtLoadingIndicator />
+    <div class="d-flex align-center justify-center h-screen">
+      <img :width="250" src="~/assets/images/athomes-logo-white.png" />
+    </div>
+  </v-app>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Athomes Realty',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/images/athomes-icon.png' },
+      ],
+    }
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
